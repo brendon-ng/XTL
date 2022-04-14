b44ae164f3db8928ad82f977b36986454dda5286
Jivan Gubbi <Jivan@jivans-mbp-2.home>
2022-04-13 23:57:27 -0700

    Initial capstone commit with parsing of json

--- src/main/java/app/transform/Transformation.java
+++ src/main/java/app/transform/Transformation.java
@@ -1,6 +1,7 @@
 package app.transform;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 
 import java.io.IOException;
 import java.lang.ProcessBuilder;
@@ -8,9 +9,17 @@ import java.lang.ProcessBuilder;
 import app.Connector;
 import app.utils.Constants;
 
+import java.util.ArrayList;
+
 public class Transformation extends Connector {
-    private String mapper;
-    private String reducer;
+    private class SingleTransform {
+        public String id;
+        public String mapper;
+        public String reducer;
+        public ArrayList<String> dependencies;
+    }
+
+    private ArrayList<SingleTransform> nodes;
 
     public Transformation(JSONObject config) {
         super(config);
@@ -18,18 +27,39 @@ public class Transformation extends Connector {
 
     @Override
     protected void parseJSON(JSONObject config) {
-        this.mapper = (String) config.get("mapper");
-        this.reducer = (String) config.get("reducer");
+        nodes = new ArrayList<SingleTransform>();
+        JSONArray entries = (JSONArray) config.get("entries");
+
+        // loop through all entries
+        for (int i = 0; i < entries.size(); i++) {
+            JSONObject value = (JSONObject) entries.get(i);
+
+            // for each entry add new node to our list of jobs
+            SingleTransform node = new SingleTransform();
+            node.id = (String) value.get("id");
+            node.mapper = (String) value.get("mapper");
+            node.reducer = (String) value.get("reducer");
+            node.dependencies = new ArrayList<String>();
+            JSONArray dependencyJson = (JSONArray) value.get("dependencies");
+            for(int j = 0; j < dependencyJson.size(); j++) {
+                node.dependencies.add((String) dependencyJson.get(j));
+            }
+
+            nodes.add(node);
+        }
     }
 
     @Override
     public void execute() {
+        // first need to topologically sort nodes based on dependencies
         try {
+            System.out.println(nodes);
+
             Process p = new ProcessBuilder("mapred", "streaming",
                     "-input", Constants.WORKING_DIR,
                     "-output", Constants.OUTGOING_DIR,
-                    "-mapper", mapper,
-                    "-reducer", reducer).start();
+                    "-mapper", "cat",
+                    "-reducer", "wc").start();
             p.waitFor();
         } catch (IOException e) {
             System.out.println(e);
